78540d7efd60fc39c89bbe6699df2370a5d776ff
Manuel (ljn0099) <manuel@picoweather.net>
2026-08-11 11:27:08 +0200

    Improve example1

--- examples/example1.c
+++ examples/example1.c
@@ -2,6 +2,7 @@
 #include "pRepCLimiter.h"
 #include "pRepCModes.h"
 #include "pRepCSystem.h"
+#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -41,23 +42,32 @@ int main(void) {
     }
 
     prepcSenderData_t senderData = {0};
-    const char *callsign = "EA1ZXZ";
-    const char *mode = PREPC_MODE_FT4;
-    uint64_t freqHz = 7145000;
+    char callsign[16];
+    char mode[16];
+    uint64_t freqHz;
+    int64_t snr;
     uint64_t flowStartSecs;
 
-    if (!prepc_system_time_unix_u64(&flowStartSecs)) {
-        printf("Error in prepc_system_time_unix_u64, %d\n", rc);
-        prepc_ctx_free(prepcCtx);
-        prepc_rate_free(prepcRateCtx);
-        return -1;
-    }
+    char line[128];
 
-    int c;
-    while ((c = getchar()) != EOF) { // Press a key to continue
+    printf("Introduce the spots with the fields separated by spaces in this order:\n");
+    printf("CALLSIGN MODE FREQHz SNR\n");
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+        if (sscanf(line, "%15s %15s %" SCNu64 " %" SCNd64, callsign, mode, &freqHz, &snr) != 4) {
+            printf("Invalid line\n");
+            continue;
+        }
+
+        // Get system time
+        if (!prepc_system_time_unix_u64(&flowStartSecs)) {
+            printf("Error in prepc_system_time_unix_u64\n");
+            prepc_ctx_free(prepcCtx);
+            prepc_rate_free(prepcRateCtx);
+            return -1;
+        }
+
         rc = prepc_rate_should_report(prepcRateCtx, callsign, strlen(callsign), mode, strlen(mode),
                                       freqHz, flowStartSecs);
-
         if (rc == PREPC_ERR_NOT_REPORT) {
             printf("Spot not reported\n");
             continue;
@@ -71,15 +81,26 @@ int main(void) {
 
         prepc_sender_data_reset(&senderData);
         prepc_sender_data_set_callsign(&senderData, callsign, strlen(callsign));
-        prepc_sender_data_set_frequency(&senderData, freqHz);
         prepc_sender_data_set_mode(&senderData, mode, strlen(mode));
+        prepc_sender_data_set_frequency(&senderData, freqHz);
+        prepc_sender_data_set_snr(&senderData, snr);
         prepc_sender_data_set_info_src(&senderData, PREPC_INFO_SRC_AUTO, false);
         prepc_sender_data_set_flow_start_secs(&senderData, flowStartSecs);
 
         rc = prepc_ctx_add_sender(prepcCtx, &senderData);
-        if (rc != PREPC_ERR_OK) { // You can try to call again the function and recover if it
-                                  // returns PREPC_ERR_NETWORK
+        // You can try to call again the function and recover if it returns PREPC_ERR_NETWORK
+        if (rc != PREPC_ERR_OK) {
             printf("Error in prepc_ctx_add_sender, %d\n", rc);
+            prepc_ctx_free(prepcCtx);
+            prepc_rate_free(prepcRateCtx);
+            return -1;
+        }
+
+        // Try to send if last was sent more than 5 minutes ago
+        rc = prepc_ctx_flush(prepcCtx, 300, false);
+        // You can try to call again the function and recover if it returns PREPC_ERR_NETWORK
+        if (rc != PREPC_ERR_OK) {
+            printf("Error in prepc_ctx_send_manually, %d\n", rc);
             prepc_ctx_free(prepcCtx);
             prepc_rate_free(prepcRateCtx);
             return -1;
@@ -87,6 +108,7 @@ int main(void) {
     }
 
     rc = prepc_ctx_flush(prepcCtx, 0, false);
+    // You can try to call again the function and recover if it returns PREPC_ERR_NETWORK
     if (rc != PREPC_ERR_OK) {
         printf("Error in prepc_ctx_send_manually, %d\n", rc);
         prepc_ctx_free(prepcCtx);
